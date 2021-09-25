c1d787135d8d598d545c827c6e46e1332a03439e
Mjtala <mjtala@uc.cl>
2021-09-25 11:06:21 -0300

    probandoAmbiente

---
+++ lib/clock_display.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'time'
+# clase clock
+class Clock
+  attr_accessor :hour, :minutes, :seconds
+
+  def initialize(hour, minutes, seconds)
+    @hour = hour
+    @minutes = minutes
+    @seconds = seconds
+  end
+
+  def print_string
+    @total_seconds = @hour * 3600 + @minutes * 60 + @seconds
+    @time = Time.at(@total_seconds).utc.strftime('%H:%M:%S')
+    @time
+  end
+
+  def increment
+    @seconds += 1
+  end
+end
