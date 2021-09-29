7e11b544d6128367d4bc0fff821bb51ff9153e26
Mjtala <mjtala@uc.cl>
2021-09-28 21:47:23 -0300

    fix: actualizar tablero

--- lib/board_controller.rb
+++ lib/board_controller.rb
@@ -1,39 +1,41 @@
+# frozen_string_literal: true
+
 class BoardController
-    def initialize(boardModel, boardView)
-        @model = boardModel
-        @view = boardView
-    end
+  def initialize(boardModel, boardView)
+    @model = boardModel
+    @view = boardView
+  end
 
-    def printBoard
-        @view.printBoard(@model)
-    end
+  def printBoard
+    @view.printBoard(@model)
+  end
 
-    def requestInput()
-        @view.requestPlay()
-        key = $stdin.gets
-        x = key.split(",")[0].to_i
-        y = key.split(",")[1].to_i
-        response = [x, y]
-        if x > @model.width || x <= 0 || y > @model.width || y <= 0 || response.length <= 1 || response.include?("\\n") || response.include?("0")
-            @view.sendErrorMessage()
-            return requestInput()
-        end
-        return response
-        ## aqui se obtiene la posicion que elige el jugador
+  def requestInput
+    @view.requestPlay
+    key = $stdin.gets
+    x = key.split(',')[0].to_i
+    y = key.split(',')[1].to_i
+    response = [x, y]
+    if x > @model.width || x <= 0 || y > @model.width || y <= 0 || response.length <= 1 || response.include?('\\n') || response.include?('0')
+      @view.sendErrorMessage
+      return requestInput
     end
+    response
+    ## aqui se obtiene la posicion que elige el jugador
+  end
 
-    def play
-        @view.printBoard(@model)
-        # aqui tienen que hacer todo el flujo del juego // miren el juego del profe
-        if @model.game_over then
-            @view.gameOver()
-            return
-        elsif @model.win()
-            @view.congratulate()      
-        else
-            input = requestInput()
-            @model.reveal(input[0], input[1])
-            play()
-        end
+  def play
+    @view.printBoard(@model)
+    # aqui tienen que hacer todo el flujo del juego // miren el juego del profe
+    if @model.game_over
+      @view.gameOver
+      nil
+    elsif @model.win
+      @view.congratulate
+    else
+      input = requestInput
+      @model.reveal(input[0], input[1])
+      play
     end
+  end
 end
--- lib/board_view.rb
+++ lib/board_view.rb
@@ -1,47 +1,52 @@
+# frozen_string_literal: true
 
 class BoardView
-    # def update(boarModel)
-    #     clean()
-    #     printBoard(boardModel)
-    # end
-    def printBoard(boardModel)
-        print " "
-        for i in  1..boardModel.width
-            print "     #{i}"
+  # def update(boarModel)
+  #     clean()
+  #     printBoard(boardModel)
+  # end
+  def printBoard(boardModel)
+    print ' '
+    (1..boardModel.width).each do |i|
+      print "     #{i}"
+    end
+    puts ''
+    position = 0
+    (1..boardModel.width).each do |i|
+      print i
+      (1..boardModel.width).each do |_j|
+        if boardModel.board[position][:revealed?]
+          print("    #{boardModel.board[position][:value]}")
+        else
+          print '    ⬜'
         end
-        puts ""
-        position = 0
-        for i in 1..boardModel.width
-            print i
-            for j in 1..boardModel.width
-                if (boardModel.board[position][:revealed?]) then
-                    print ("    #{boardModel.board[position][:value]}")
-                else
-                    print "    ⬜"
-                end
-                position += 1
-            end
-            puts ""
-        end
+        position += 1
+      end
+      puts ''
     end
-    def startGame()
-        puts()
-        puts "Bienvenido a Minesweeper!"
-        puts "Para jugar debes ingresar los números de la posición (fila, columna) con un espacio  que quieres descubrir."
-        puts "Aquí está el tablero:"
-    end
-    def congratulate()
-        puts "Felicitaciones has ganado!"
-    end
-    def gameOver()
-        puts "Explotó una bomba! Haz perdido :("
-    end
+    STDOUT.flush
+  end
 
-    def requestPlay()
-        puts "Seleccióna tu jugada"
-    end
+  def startGame
+    puts
+    puts 'Bienvenido a Minesweeper!'
+    puts 'Para jugar debes ingresar los números de la posición (fila, columna) con un espacio  que quieres descubrir.'
+    puts 'Aquí está el tablero:'
+  end
 
-    def sendErrorMessage()
-        puts "Has introducido una jugada invalida"
-    end
+  def congratulate
+    puts 'Felicitaciones has ganado!'
+  end
+
+  def gameOver
+    puts 'Explotó una bomba! Haz perdido :('
+  end
+
+  def requestPlay
+    puts 'Seleccióna tu jugada'
+  end
+
+  def sendErrorMessage
+    puts 'Has introducido una jugada invalida'
+  end
 end
