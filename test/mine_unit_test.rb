716756a05dd36850234d9fbf53dcfc317ef11d4f
fivaldivia <fivaldivia@uc.cl>
2021-09-29 16:45:30 -0300

    Test funcion reveal() game_over del modelo listo

--- test/mine_unit_test.rb
+++ test/mine_unit_test.rb
@@ -7,10 +7,10 @@ require 'test/unit'
 
 class MineUnitTest < Test::Unit::TestCase
    
-  def test_define_bordering
-       @board = Board.new()
-       output = @board.define_bordering
-       expected = [
+    def test_define_bordering
+        @board = Board.new()
+        output = @board.define_bordering
+        expected = [
         [-1, -1], # upper-left
         [-1, 0], # top
         [-1, 1], # upper-right
@@ -19,22 +19,41 @@ class MineUnitTest < Test::Unit::TestCase
         [1, 0], # bottom
         [1, -1], # lower-left
         [0, -1] # left
-      ]
-      assert_equal(expected, output)
-   end
-
-  def test_win
-    new_board = Board.new()
-    board = new_board.board
-    (0..board.length-1).each do |box_index|
-      if board[box_index-1][:value] == '💣'
-        next
-      else
-        # Si no es bomba lo revelamos
-        board[box_index][:revealed?] = true
-      end
+        ]
+        assert_equal(expected, output)
     end
-    assert_true(new_board.win())
-  end
+
+    def test_continue_playing
+
+    end
+
+    
+    def test_game_over
+        new_board = Board.new()
+        board = new_board.board
+        (0..board.length-1).each do |box_index|
+            if board[box_index][:value] == '💣'
+                board[box_index][:revealed?] = true
+                row, col = new_board.get_coordinates(box_index)
+                new_board.reveal(row, col)
+                break
+            else
+                next
+            end
+        end
+        assert_true(new_board.game_over())
+    end
+
+    def test_win
+        new_board = Board.new()
+        board = new_board.board
+        (0..board.length-1).each do |box_index|
+            if board[box_index][:value] != '💣'
+                # Si no es bomba lo revelamos
+                board[box_index][:revealed?] = true
+            end
+        end
+        assert_true(new_board.win())
+    end
 
 end
