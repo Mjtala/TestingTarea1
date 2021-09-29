849066b03ee80071a9c3dd4fd5af946f890df3ba
Mjtala <mjtala@uc.cl>
2021-09-29 15:31:26 -0300

    chequeando rubocop

--- Rakefile
+++ Rakefile
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-require "rubocop/rake_task"
+require 'rubocop/rake_task'
 
 task default: %w[test]
 task :test do
-  ruby "test/mine_unit_test.rb"
+  ruby 'test/mine_unit_test.rb'
   # Acá ponemos todos los tests que tengamos
 end
 
 RuboCop::RakeTask.new(:lint) do |task|
-  task.patterns = ["lib/**/*.rb", "test/**/*.rb"]
+  task.patterns = ['lib/**/*.rb', 'test/**/*.rb']
   task.fail_on_error = false
 end
--- test/mine_unit_test.rb
+++ test/mine_unit_test.rb
@@ -6,26 +6,36 @@ require_relative '../lib/board_model'
 require 'test/unit'
 
 class MineUnitTest < Test::Unit::TestCase
-   
   def test_define_bordering
-       @board = Board.new()
-       output = @board.define_bordering
-       expected = [
-        [-1, -1], # upper-left
-        [-1, 0], # top
-        [-1, 1], # upper-right
-        [0, 1], # right
-        [1, 1], # lower-right
-        [1, 0], # bottom
-        [1, -1], # lower-left
-        [0, -1] # left
-      ]
-      assert_equal(expected, output)
-   end
+    @board = Board.new
+    output = @board.define_bordering
+    expected = [
+      [-1, -1], # upper-left
+      [-1, 0], # top
+      [-1, 1], # upper-right
+      [0, 1], # right
+      [1, 1], # lower-right
+      [1, 0], # bottom
+      [1, -1], # lower-left
+      [0, -1] # left
+    ]
+    assert_equal(expected, output)
+  end
 
   def test_win
-    @board = Board.new()
+    @board = Board.new
     # revisar como verificar posicion de bombas
   end
 
+  def test_define_bordering_cross
+    @board = Board.new
+    output = @board.define_bordering_cross
+    expected = [
+      [-1, 0], # top
+      [0, 1], # right
+      [1, 0], # bottom
+      [0, -1] # left
+    ]
+    assert_equal(expected, output)
+  end
 end
